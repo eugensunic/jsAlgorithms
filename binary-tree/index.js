6f7cf204d94b358ba5478ada29206e36858d5cef
Eugen Sunic <eugen.sunic@kindredgroup.com>
2020-11-19 17:15:11 +0000

    implement BFS and insertnode

--- binary-tree/index.js
+++ binary-tree/index.js
@@ -10,34 +10,41 @@ class BinaryTree {
     constructor() {
         this.root = null
     }
-
     add(data) {
-        if (!this.root) {
-            this.root = new Node(data)
+        var newNode = new Node(data);
+        if (this.root === null)
+            this.root = newNode;
+        else
+            this.insertNode(this.root, newNode);
+    }
+
+
+    traverse(node, factorLeft, factorRight) {
+        if (!node.right || !node.left) {
             return;
         }
-        this.addItem(data, this.root)
+        console.log(node.data);
+        console.log('/'.padStart(2 * (++factorRight), ' '));
+        this.traverse(node.left, ++factorLeft, factorRight);
+        this.traverse(node.right, factorLeft, ++factorRight);
+
 
     }
 
-    print() {
-        console.log(this.root)
-    }
-
-    addItem(data, node) {
-        if (!node.right) {
-            node.right = new Node(data);
-            return;
-        }
+    insertNode(node, newNode) {
         if (!node.left) {
-            node.left = new Node(data);
+            node.left = newNode
             return;
         }
-        if (data < node.data) {
-            this.addItem(data, node.left);
+        if (!node.right) {
+            node.right = newNode
+            return;
         }
-        if (data >= node.data) {
-            this.addItem(data, node.right);
+        if (newNode.data < node.data) {
+            this.insertNode(node.left, newNode)
+        }
+        if (newNode.data > node.data) {
+            this.insertNode(node.right, newNode)
         }
     }
 
@@ -48,22 +55,58 @@ class BinaryTree {
         }
         return this.hasData(data, node.left) || this.hasData(data, node.right);
 
+    }
 
+    findMin(node) {
+        if (!node.left) {
+            return node.data
+        }
+        return this.findMin(node.left)
+    }
+
+    findMax(node) {
+        if (!node.right) {
+            return node.data
+        }
+        return this.findMin(node.right)
+    }
+
+    traverseBFS(root) {
+        let queue = [root]
+        let res = []
+        let i = 0;
+        while (queue.length) {
+            let curr = queue.shift()
+            if (curr.data < (res[res.length - 1] && res[res.length - 1].data)) {
+                i++;
+            }
+            res.push({ data: curr.data, level: i });
+            if (curr.left) {
+                queue.push(curr.left)
+            }
+
+            if (curr.right) {
+                queue.push(curr.right)
+            }
+
+        }
+
+        return res
     }
 
 }
 
 // invoke BinaryTree
 const binaryTree = new BinaryTree();
+binaryTree.add(8);
 binaryTree.add(5);
 binaryTree.add(10);
-binaryTree.add(3);
-binaryTree.add(4);
 binaryTree.add(2);
+binaryTree.add(6);
+binaryTree.add(9);
+binaryTree.add(15);
+console.log(binaryTree);
 
-binaryTree.print();
-console.log(binaryTree.hasData(55, binaryTree.root))
-
-
+console.log(binaryTree.traverseBFS(binaryTree.root));
 
 
